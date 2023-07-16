71bc1f3290342b49c218f9cf3c2bce89cc6eeff1
BMMUGOMBA <bmmugomba@gmail.com>
2023-07-17 01:07:22 +0200

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -8,32 +8,26 @@ from models import storage
 
 
 class BaseModel():
-    """ Defines all common attributes/methods for other classes """
+    """ Defines all common attributes and methods for other classes that inherits from the class """
     def __init__(self, *args, **kwargs):
-        """ Initializes the instances attributes """
+        """ initialization of class instance attributes """
         if kwargs:
-            date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            k_dict = kwargs.copy()
-            del k_dict["__class__"]
-            for key in k_dict:
+            date_time = "%Y-%m-%dT%H:%M:%S.%f"
+            arg_dict = kwargs.copy()
+            del arg_dict["__class__"]
+            for key in arg_dict:
                 if (key == "created_at" or key == "updated_at"):
-                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
-            self.__dict__ = k_dict
+                    arg_dict[key] = datetime.strptime(arg_dict[key], date_time)
+            self.__dict__ = arg_dict
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
             storage.new(self)
 
-    def __str__(self):
-        """ Prints object in friendly format"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+    
 
-    def save(self):
-        """ Updates update_at """
-        self.updated_at = datetime.today()
-        storage.save()
+    
 
     def to_dict(self):
         """ Generate a new dict with an extra field __class__ """
@@ -42,3 +36,15 @@ class BaseModel():
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
+
+
+    def save(self):
+        """ Updates update_at """
+        self.updated_at = datetime.today()
+        storage.save()
+
+    
+    def __str__(self):
+        """ Prints object in friendly format starting with class name followed by id and otehr attributes"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
