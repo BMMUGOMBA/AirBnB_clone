9fc32028ad88bc5e7d0261d26da7dbc06975dfda
BMMUGOMBA <bmmugomba@gmail.com>
2023-07-16 17:39:56 +0200

    basemodel edit

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,26 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        #self.created_at = datetime.now()
+        #self.updated_at = self.created_at
+        
+        now = datetime.now()
+    
+        self.created_at = now
+        self.updated_at = now
 
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        
+        # Get the name of the class of the object
+        class_name = type(self).__name__
+        # Get the id of the object
+        id = self.id
+        # Get the dictionary of attributes of the object
+        attributes = self.__dict__
+        # Return a formatted string that contains the class name, id, and attributes of the object
+        return f"[{class_name}] ({id}) {attributes}"
+        #return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
 
     def save(self):
